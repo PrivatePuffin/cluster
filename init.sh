645a42f44d488aa768f2ba82f478e682583d5e81
Kjeld Schouten <kjeld@schouten-lebbing.nl>
2023-10-29 23:29:24 +0100

    move more config to all patch

--- init.sh
+++ init.sh
@@ -5,9 +5,15 @@ controlIP1=10.0.40.1/24
 controlIP2=10.0.40.2/24
 controlIP3=10.0.40.3/24
 
-# Uncomment to generate new node configurations
-talosctl gen config "main" "https://$VIP:6443" --config-patch @patches/all.json --config-patch-control-plane @patches/controlplane.json --config-patch-worker @patches/worker.json -o config --force
+# Generate Talos Secrets
+talosctl gen secrets -o talos.secret
 
+# Uncomment to generate new node configurations
+talosctl gen config "main" "https://$VIP:6443" --config-patch-control-plane @patches/init/controlplane.json --config-patch-worker @patches/init/worker.json -o config --with-secrets talos.secret --force
+
+talosctl machineconfig patch config/controlplane.yaml --patch @patches/update/controlplane.json -o config/controlplane.yaml
+
+talosctl machineconfig patch config/worker.yaml --patch @patches/update/worker.json -o config/worker.yaml
 
 # Control plane configuration
 cat config/controlplane.yaml | sed -e "s|!!HOSTNAME!!|k8s-control-1|" > config/controlplane/k8s-control-1.yaml
