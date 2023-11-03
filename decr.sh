95ec0cb866b4f98001705e7e0ce564542a9525c9
Kjeld Schouten <kjeld@schouten-lebbing.nl>
2023-11-03 18:36:10 +0100

    add encryption and decryption scripts

--- decr.sh
+++ decr.sh
@@ -1,2 +1,4 @@
-SOPS_AGE_KEY_FILE=age.key
+export SOPS_AGE_KEY_FILE="age.key"
 sops -d -i talos.secret
+sops -d -i patches/custom/controlplane.json patches/custom/worker.json
+find ./config -type f -exec sops -d -i {} \;
--- enc.sh
+++ enc.sh
@@ -1,2 +1,4 @@
-SOPS_AGE_KEY_FILE="age.key"
-sops -d -i talos.secret
+export SOPS_AGE_KEY_FILE="age.key"
+sops --encrypt -i talos.secret
+sops --encrypt -i patches/custom/controlplane.json patches/custom/worker.json
+find ./config -type f -exec sops --encrypt -i {} \;
