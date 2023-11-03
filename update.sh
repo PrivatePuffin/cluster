1d5982ab4e5abce3f1170a0495620a097ab23cb0
Kjeld Schouten <kjeld@schouten-lebbing.nl>
2023-11-03 15:43:48 +0100

    add initial support for multirack and edge workers

--- update.sh
+++ update.sh
@@ -9,23 +9,37 @@ talosctl machineconfig patch config/worker.yaml --patch @patches/update/worker.j
 talosctl machineconfig patch config/controlplane.yaml --patch @patches/custom/controlplane.json -o config/controlplane.yaml
 talosctl machineconfig patch config/worker.yaml --patch @patches/custom/worker.json -o config/worker.yaml
 
+LOCATION="main"
 # Control plane configuration
-cat config/controlplane.yaml | sed -e "s|!!HOSTNAME!!|k8s-control-1|" > config/controlplane/k8s-control-1.yaml
-cat config/controlplane.yaml | sed -e "s|!!VIP!!|$VIP|" > config/controlplane/k8s-control-1.yaml
-
-cat config/controlplane.yaml | sed -e "s|!!HOSTNAME!!|k8s-control-2|" > config/controlplane/k8s-control-2.yaml
-cat config/controlplane.yaml | sed -e "s|!!VIP!!|$VIP|" > config/controlplane/k8s-control-2.yaml
-
-cat config/controlplane.yaml | sed -e "s|!!HOSTNAME!!|k8s-control-3|" > config/controlplane/k8s-control-3.yaml
-cat config/controlplane.yaml | sed -e "s|!!VIP!!|$VIP|" > config/controlplane/k8s-control-3.yaml
+cat config/controlplane.yaml | sed -e "s|!!HOSTNAME!!|k8s-control-1|" | sed -e "s|!!VIP!!|$VIP|" | sed -e "s|!!LOCATION!!|$LOCATION|" | sed -e "s|!!RACKID!!|rack-a|" > config/controlplane/k8s-control-a1.yaml
+cat config/controlplane.yaml | sed -e "s|!!HOSTNAME!!|k8s-control-2|" | sed -e "s|!!VIP!!|$VIP|" | sed -e "s|!!LOCATION!!|$LOCATION|" | sed -e "s|!!RACKID!!|rack-b|" > config/controlplane/k8s-control-b1.yaml
+cat config/controlplane.yaml | sed -e "s|!!HOSTNAME!!|k8s-control-3|" | sed -e "s|!!VIP!!|$VIP|" | sed -e "s|!!LOCATION!!|$LOCATION|" | sed -e "s|!!RACKID!!|rack-c|" > config/controlplane/k8s-control-c1.yaml
 
 # Worker configuration
-cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-worker-1/" > config/workers/k8s-worker-A-1.yaml
-cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-worker-1/" > config/workers/k8s-worker-A-2.yaml
-cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-worker-1/" > config/workers/k8s-worker-A-3.yaml
-cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-worker-2/" > config/workers/k8s-worker-B-1.yaml
-cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-worker-2/" > config/workers/k8s-worker-B-2.yaml
-cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-worker-2/" > config/workers/k8s-worker-B-3.yaml
-cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-worker-3/" > config/workers/k8s-worker-C-1.yaml
-cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-worker-3/" > config/workers/k8s-worker-C-2.yaml
-cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-worker-3/" > config/workers/k8s-worker-C-3.yaml
+RACK="rack-a"
+cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-worker-a1/" | sed -e "s|!!LOCATION!!|$LOCATION|" | sed -e "s|!!RACKID!!|$RACK|"  > config/workers/k8s-worker-a1.yaml
+cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-worker-a2/" | sed -e "s|!!LOCATION!!|$LOCATION|" | sed -e "s|!!RACKID!!|$RACK|"  > config/workers/k8s-worker-a2.yaml
+cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-worker-a3/" | sed -e "s|!!LOCATION!!|$LOCATION|" | sed -e "s|!!RACKID!!|$RACK|"  > config/workers/k8s-worker-a3.yaml
+RACK="rack-b"
+cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-worker-b1/" | sed -e "s|!!LOCATION!!|$LOCATION|" | sed -e "s|!!RACKID!!|$RACK|"  > config/workers/k8s-worker-b1.yaml
+cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-worker-b2/" | sed -e "s|!!LOCATION!!|$LOCATION|" | sed -e "s|!!RACKID!!|$RACK|"  > config/workers/k8s-worker-b2.yaml
+cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-worker-b3/" | sed -e "s|!!LOCATION!!|$LOCATION|" | sed -e "s|!!RACKID!!|$RACK|"  > config/workers/k8s-worker-b3.yaml
+RACK="rack-c"
+cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-worker-c1/" | sed -e "s|!!LOCATION!!|$LOCATION|" | sed -e "s|!!RACKID!!|$RACK|"  > config/workers/k8s-worker-c1.yaml
+cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-worker-c2/" | sed -e "s|!!LOCATION!!|$LOCATION|" | sed -e "s|!!RACKID!!|$RACK|"  > config/workers/k8s-worker-c2.yaml
+cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-worker-c3/" | sed -e "s|!!LOCATION!!|$LOCATION|" | sed -e "s|!!RACKID!!|$RACK|"  > config/workers/k8s-worker-c3.yaml
+
+# edge worker configuration
+LOCATION="edge"
+RACK="rack-d"
+cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-edge-a1/" | sed -e "s|!!LOCATION!!|$LOCATION|" | sed -e "s|!!RACKID!!|$RACK|"  > config/edge-workers/k8s-edge-workers-a1.yaml
+cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-edge-a2/" | sed -e "s|!!LOCATION!!|$LOCATION|" | sed -e "s|!!RACKID!!|$RACK|"  > config/edge-workers/k8s-edge-workers-a2.yaml
+cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-edge-a3/" | sed -e "s|!!LOCATION!!|$LOCATION|" | sed -e "s|!!RACKID!!|$RACK|"  > config/edge-workers/k8s-edge-workers-a3.yaml
+RACK="rack-e"
+cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-edge-b1/" | sed -e "s|!!LOCATION!!|$LOCATION|" | sed -e "s|!!RACKID!!|$RACK|"  > config/edge-workers/k8s-edge-workers-b1.yaml
+cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-edge-b2/" | sed -e "s|!!LOCATION!!|$LOCATION|" | sed -e "s|!!RACKID!!|$RACK|"  > config/edge-workers/k8s-edge-workers-b2.yaml
+cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-edge-b3/" | sed -e "s|!!LOCATION!!|$LOCATION|" | sed -e "s|!!RACKID!!|$RACK|"  > config/edge-workers/k8s-edge-workers-b3.yaml
+RACK="rack-f"
+cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-edge-c1/" | sed -e "s|!!LOCATION!!|$LOCATION|" | sed -e "s|!!RACKID!!|$RACK|"  > config/edge-workers/k8s-edge-workers-c1.yaml
+cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-edge-c2/" | sed -e "s|!!LOCATION!!|$LOCATION|" | sed -e "s|!!RACKID!!|$RACK|"  > config/edge-workers/k8s-edge-workers-c2.yaml
+cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-edge-c3/" | sed -e "s|!!LOCATION!!|$LOCATION|" | sed -e "s|!!RACKID!!|$RACK|"  > config/edge-workers/k8s-edge-workers-c3.yaml
