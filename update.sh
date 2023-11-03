d2c1b5ea1db586e4cc0a5ab8476e20d5a44af289
Kjeld Schouten <kjeld@schouten-lebbing.nl>
2023-11-03 12:29:35 +0100

    tweak more settings, add initial support for extentions and allow for users to create custom override patches Also remove genered config

--- update.sh
+++ update.sh
@@ -1,9 +1,13 @@
 
 echo "patching config..."
 
+# Apply update patches
 talosctl machineconfig patch config/controlplane.yaml --patch @patches/update/controlplane.json -o config/controlplane.yaml
-
 talosctl machineconfig patch config/worker.yaml --patch @patches/update/worker.json -o config/worker.yaml
+
+# Apply custom user patches
+talosctl machineconfig patch config/controlplane.yaml --patch @patches/custom/controlplane.json -o config/controlplane.yaml
+talosctl machineconfig patch config/worker.yaml --patch @patches/custom/worker.json -o config/worker.yaml
 
 # Control plane configuration
 cat config/controlplane.yaml | sed -e "s|!!HOSTNAME!!|k8s-control-1|" > config/controlplane/k8s-control-1.yaml
@@ -16,6 +20,12 @@ cat config/controlplane.yaml | sed -e "s|!!HOSTNAME!!|k8s-control-3|" > config/c
 cat config/controlplane.yaml | sed -e "s|!!VIP!!|$VIP|" > config/controlplane/k8s-control-3.yaml
 
 # Worker configuration
-cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-worker-1/" > config/workers/k8s-worker-1.yaml
-cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-worker-2/" > config/workers/k8s-worker-2.yaml
-cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-worker-3/" > config/workers/k8s-worker-3.yaml
+cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-worker-1/" > config/workers/k8s-worker-A-1.yaml
+cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-worker-1/" > config/workers/k8s-worker-A-2.yaml
+cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-worker-1/" > config/workers/k8s-worker-A-3.yaml
+cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-worker-2/" > config/workers/k8s-worker-B-1.yaml
+cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-worker-2/" > config/workers/k8s-worker-B-2.yaml
+cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-worker-2/" > config/workers/k8s-worker-B-3.yaml
+cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-worker-3/" > config/workers/k8s-worker-C-1.yaml
+cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-worker-3/" > config/workers/k8s-worker-C-2.yaml
+cat config/worker.yaml | sed -e "s/!!HOSTNAME!!/k8s-worker-3/" > config/workers/k8s-worker-C-3.yaml
