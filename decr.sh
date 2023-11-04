1d56b30ea2f2b150dfcaebccd46648b5eae41560
Kjeld Schouten <kjeld@schouten-lebbing.nl>
2023-11-04 13:50:24 +0100

    prevent double encryption and don't encrypt whole files

--- decr.sh
+++ decr.sh
@@ -1,4 +1,24 @@
+#!/bin/bash
+
 export SOPS_AGE_KEY_FILE="age.key"
-sops -d -i talos.secret
-sops -d -i patches/custom/controlplane.json patches/custom/worker.json
-find ./config -type f -exec sops -d -i {} \;
+FILES=('patches/custom/controlplane.json' 'patches/custom/worker.json' 'config/talosconfig')
+
+while IFS=  read -r -d $'\0'; do
+    FILES+=("$REPLY")
+done < <(find . -name "*.yaml" -type f -print0)
+
+while IFS=  read -r -d $'\0'; do
+    FILES+=("$REPLY")
+done < <(find . -name "*.secret" -type f -print0)
+
+
+if test -f "ENCRYPTED"; then
+  for value in "${FILES[@]}"
+  do
+    echo "$value"
+    sops -d -i "$value" || echo "skipping..."
+  done
+  rm -f ENCRYPTED
+else
+  echo "ERROR DATA ALREADY DECRYPTED"
+fi
--- enc.sh
+++ enc.sh
@@ -1,4 +1,24 @@
+#!/bin/bash
+
 export SOPS_AGE_KEY_FILE="age.key"
-sops --encrypt -i talos.secret
-sops --encrypt -i patches/custom/controlplane.json patches/custom/worker.json
-find ./config -type f -exec sops --encrypt -i {} \;
+
+FILES=('patches/custom/controlplane.json' 'patches/custom/worker.json' 'config/talosconfig')
+while IFS=  read -r -d $'\0'; do
+    FILES+=("$REPLY")
+done < <(find . -name "*.yaml" -type f -print0)
+
+while IFS=  read -r -d $'\0'; do
+    FILES+=("$REPLY")
+done < <(find . -name "*.secret" -type f -print0)
+
+
+if test -f "ENCRYPTED"; then
+  echo "ERROR DATA ALREADY ENCRYPTED"
+else
+  for value in "${FILES[@]}"
+  do
+    echo "$value"
+    sops --encrypt -i "$value" || echo "skipping..."
+  done
+  touch ENCRYPTED
+fi
