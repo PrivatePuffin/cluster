ceee9f7d1e6cd817dd59bb1aa2e48271815bda74
Kjeld Schouten <kjeld@schouten-lebbing.nl>
2023-11-25 19:36:09 +0100

    fix extra deply

--- deps/deploy-extras.sh
+++ deps/deploy-extras.sh
@@ -1,28 +1,21 @@
 #!/usr/bin/sudo bash
 
 deploy_cni(){
-if test -d ./deps/cni/charts; then
-  rm -rf ./deps/cni/charts
-fi
-cat ./cluster/kube-system/cilium/cilium-values.yaml > ./deps/cni/values.yaml
+rm -rf ./deps/cni/charts || true
+cat ./cluster/kube-system/cilium/app/cilium-values.yaml > ./deps/cni/values.yaml
 kustomize build --enable-helm ./deps/cni | kubectl apply -f -
-rm ./deps/cni/values.yaml
-if test -d ./deps/csr-approver/charts; then
-  rm -rf ./deps/csr-approver/charts
-fi
+rm -f ./deps/cni/values.yaml || true
+rm -rf ./deps/csr-approver/charts || true
 }
 export deploy_cni
 
 deploy_approver(){
-if test -d ./deps/csr-approver/charts; then
-  rm -rf ./deps/csr-approver/charts
-fi
-cat ./cluster/kube-system/kubelet-csr-approver/values.yaml > ./deps/csr-approver/values.yaml
+rm -rf ./deps/csr-approver/charts || true
+cat ./cluster/kube-system/kubelet-csr-approver/app/values.yaml > ./deps/csr-approver/values.yaml
 kustomize build --enable-helm ./deps/csr-approver | kubectl apply -f -
-rm ./deps/csr-approver/values.yaml
-if test -d ./deps/csr-approver/charts; then
-  rm -rf ./deps/csr-approver/charts
-fi
+
+rm -f ./deps/csr-approver/values.yaml || true
+rm -rf ./deps/csr-approver/charts || true
 popd >/dev/null 2>&1
 }
 export deploy_approver
