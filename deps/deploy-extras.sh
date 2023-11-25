bd973a2fe25ff8d6c349d44f278da57917d6fcfd
Kjeld Schouten <kjeld@schouten-lebbing.nl>
2023-11-25 20:05:39 +0100

    ok

--- deps/deploy-extras.sh
+++ deps/deploy-extras.sh
@@ -2,6 +2,7 @@
 
 deploy_cni(){
 rm -rf ./deps/cni/charts || true
+rm -f ./deps/cni/values.yaml || true
 cat ./cluster/kube-system/cilium/app/cilium-values.yaml > ./deps/cni/values.yaml
 kustomize build --enable-helm ./deps/cni | kubectl apply -f -
 rm -f ./deps/cni/values.yaml || true
@@ -11,10 +12,7 @@ export deploy_cni
 
 deploy_approver(){
 rm -rf ./deps/csr-approver/charts || true
-cat ./cluster/kube-system/kubelet-csr-approver/app/values.yaml > ./deps/csr-approver/values.yaml
 kustomize build --enable-helm ./deps/csr-approver | kubectl apply -f -
-
-rm -f ./deps/csr-approver/values.yaml || true
 rm -rf ./deps/csr-approver/charts || true
 popd >/dev/null 2>&1
 }
